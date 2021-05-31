fe5375b019f362d090b74ac3806a04241be4594a
timofeya123 <timofeya123@inbox.ru>
2021-05-31 21:06:12 +0600

    DragAndDrop_comment

--- src/com/AWT/Main.java
+++ src/com/AWT/Main.java
@@ -3,25 +3,25 @@ package com.AWT;
 import javax.swing.*;
 
 public class Main {
-    static JFrame jFrame = getFrame();
-    static  JPanel jPanel = new JPanel();
+    static JFrame jFrame = getFrame(); // создание формы
+    static  JPanel jPanel = new JPanel(); // создание панели
 
     public static void main(String[] args) {
-	    jFrame.add(jPanel);
-	    JTextField jTextField =new JTextField(10);
-        JTextField jTextField2 =new JTextField(10);
-        jTextField.setDragEnabled(true);
+	    jFrame.add(jPanel); // добавить панель на форму
+	    JTextField jTextField =new JTextField(20); // тестовое поле
+        JTextField jTextField2 =new JTextField(20);
+        jTextField.setDragEnabled(true); // разрешени на перетащить и бросить
         jTextField2.setDragEnabled(true);
-        jPanel.add(jTextField);
+        jPanel.add(jTextField); // добавить на панель
         jPanel.add(jTextField2);
-        jFrame.revalidate();;
+        jFrame.revalidate(); // перерисовать
     }
 
     static JFrame getFrame(){
-        JFrame jFrame=new JFrame(){};
-        jFrame.setVisible(true);
-        jFrame.setBounds(750,250,500,500);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        return jFrame;
+        JFrame jFrame=new JFrame(){}; // создание формы
+        jFrame.setVisible(true); // видимость
+        jFrame.setBounds(550,250,300,100); // размеры
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // выход
+        return jFrame; // передать форму
     }
 }
